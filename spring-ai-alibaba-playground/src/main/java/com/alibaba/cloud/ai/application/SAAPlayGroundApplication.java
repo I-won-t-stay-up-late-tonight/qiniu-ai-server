2208eb5b71707e43a741f56cddb019568240266e
jiping-zeng <2066176114@qq.com>
2025-09-23 01:47:34 +0800

    feat:框架搭建

---
+++ spring-ai-alibaba-playground/src/main/java/com/alibaba/cloud/ai/application/SAAPlayGroundApplication.java
@@ -0,0 +1,19 @@
+package com.alibaba.cloud.ai.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yuluo
+ * @author <a href="mailto:yuluo08290126@gmail.com">yuluo</a>
+ */
+
+@SpringBootApplication
+public class SAAPlayGroundApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SAAPlayGroundApplication.class, args);
+	}
+
+}
