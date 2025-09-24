ff3dd486f25dd90b0c261f70188d4457eedf7a57
IFundo <yy903825821@qq.com>
2025-09-24 16:47:25 +0800

    feat:新建角色接口

---
+++ server-service/src/main/java/com/qiniuai/chat/demos/web/service/impl/RoleServiceImpl.java
@@ -0,0 +1,39 @@
+package com.qiniuai.chat.demos.web.service.impl;
+
+import com.qiniuai.chat.demos.web.entity.pojo.Role;
+import com.qiniuai.chat.demos.web.mapper.ConversationMapper;
+import com.qiniuai.chat.demos.web.mapper.ConversationRoleRelationMapper;
+import com.qiniuai.chat.demos.web.mapper.RoleMapper;
+import com.qiniuai.chat.demos.web.service.ConversationService;
+import com.qiniuai.chat.demos.web.service.RoleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
+
+/**
+ * @ClassName audioServiceImpl
+ * @Description TODO
+ * @Author IFundo
+ * @Date 00:06 2025/9/23
+ * @Version 1.0
+ */
+
+@Service
+public class RoleServiceImpl implements RoleService {
+
+    @Autowired
+    private RoleMapper roleMapper;
+
+    @Override
+    public String createRole(Role role) {
+        if (role.getVoice() == null || role.getVoice().trim().isEmpty()) {
+            role.setVoice("Ethan"); // 或 "未设置声音"，根据业务需求定义
+        }
+        int rows = roleMapper.insertRole(role);
+        if (rows == 1){
+            return "创建成功";
+        }else {
+            return "创建失败";
+        }
+    }
+}
