16fd4692481683ec0a902c246189b1ce6506aef1
jiping-zeng <2066176114@qq.com>
2025-09-27 22:24:30 +0800

    feat:创建角色报错

--- server-service/src/main/java/com/qiniuai/chat/web/controller/RoleController.java
+++ server-service/src/main/java/com/qiniuai/chat/web/controller/RoleController.java
@@ -6,10 +6,7 @@ import com.qiniuai.chat.web.service.RoleService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @ClassName RoleController
